2e0ae4ccd4a90ccbb81cee6c8f1f694c491066aa
yammouni <yammouni@adobe.com>
2018-10-18 14:08:46 +0200

    Added 7 simultaneous lasers square (hold | staccato) / pwm.

--- Wahoo.ino/Wahoo.ino.ino
+++ Wahoo.ino/Wahoo.ino.ino
@@ -1,34 +1,47 @@
 #include <TimerOne.h>
 
 int beep = 9;
-int pint=A0;//Photoresistor R1 is connected to A0 Pin
-int pint2=A1;//Photoresistor R2 is connected to A1 Pin
-int pint3=A2;//Photoresistor R3 is connected to A2 Pin
-int pint4=A3;//Photoresistor R4 is connected to A3 Pin
-int pint5=A4;//Photoresistor R5 is connected to A4 Pin
-int pint6=A5;//Photoresistor R6 is connected to A5 Pin
-int pint7=7;//Photoresistor R7 is connected to D7 Pin
-int val;//Save A0 analog value
-int val2;//Save A1 analog value
-int val3;//Save A2 analog value
-int val4;//Save A3 analog value
-int val5;//Save A4 analog value
-int val6;//Save A5 analog value
-int val7;//Save D7 digital value
 int buttonPin = 2; //Button 1 is connected to the D2 pin
 int buttonPin1 = 3;//Button 2 is connected to the D3 pin
 int buttonPin2 = 4;//Button 3 is connected to the D4 pin
 int laserPin=12;//The transistor that controls the laser on and off is connected to the D12 pin
 int sensi=950;//Set the sensitivity of the laser string(tone) R1 pin
+const unsigned short int sampleRate = 20000;
 char totalValue = 0;
+const int lasersPin[7] = {
+ A0,
+ A1,
+ A2,
+ A3,
+ A4,
+ A5,
+ 7
+};
+
+const unsigned short int periods[7] = {
+  sampleRate / 380,
+  sampleRate / 445,
+  sampleRate / 505,
+  sampleRate / 590,
+  sampleRate / 668,
+  sampleRate / 745,
+  sampleRate / 880
+};
 
 struct SquareGenerator {
-  SquareGenerator():volume(0), phase(0), value(0){}
   unsigned short int period;
   char volume;
   char phase;
   char value;
   unsigned short int counter;
+  void init(const unsigned short int newPeriod)
+  {
+    volume = 0;
+    value = 0;    
+    phase = 0;
+    period = newPeriod;
+    counter = 0;
+  }
   void counterInc() {counter++;}
   char getValue()
   {
@@ -45,66 +58,163 @@ struct SquareGenerator {
   }
 };
 
-SquareGenerator squareGenerator[2];
+struct PwmGenerator {
+  unsigned short int periodHigh;
+  unsigned short int periodLow;
+  char volume;
+  char phase;
+  char sweepPhase;
+  char value;
+  unsigned short int counter;
+  const unsigned short int periodMin = 5;
+  void init(const unsigned short int newPeriod)
+  {
+    volume = 0;
+    value = 0;    
+    phase = 0;
+    sweepPhase = 0;
+    periodHigh = periodMin;
+    periodLow = newPeriod - periodMin;
+    counter = 0;
+  }
+  void pwmSweep()
+  {
+    if (sweepPhase == 0)
+    {
+      periodLow--;
+      periodHigh++;
+      if (periodLow < periodMin)
+        sweepPhase = 1;
+    }
+    else
+    {
+      periodLow++;
+      periodHigh--;
+      if (periodHigh < periodMin)
+        sweepPhase = 0;
+    }
+  }
+  void counterInc() {counter++;}
+  char getValue()
+  {
+    if (phase == 0)
+    {
+      if (counter >= periodHigh)
+      {
+        phase = 1;
+        value = volume;
+        counter = 0;        
+      }
+    }
+    else
+    {
+      if (counter >= periodLow)
+      {
+        phase = 0;
+        value = -volume;
+        counter = 0;
+      }
+    }
+    return value;
+  }
+};
+
+SquareGenerator squareGenerator[7];
+PwmGenerator pwmGenerator[7];
+char lasersPressed[7];
+char lasersHold[7];
 
 void setup()
 {
+  for (int i = 0; i < 7; ++i)
+  {
+    squareGenerator[i].init(periods[i]);
+    pwmGenerator[i].init(periods[i]);
+  }  
   Timer1.initialize(50);              // initialize timer1 10KHz
   Timer1.pwm(beep, 512);               // setup pwm on pin 9
-  Timer1.attachInterrupt(timerIsr);   // attaches timerIsr() as a timer overflow interrupt
-  pinMode(pint7,INPUT);//Set the D7 pin to input mode
+  Timer1.attachInterrupt(timerIsrPwm);   // attaches timerIsr() as a timer overflow interrupt
+  pinMode(7,INPUT);//Set the D7 pin to input mode
   pinMode(buttonPin, INPUT);//Set D2 pin to input mode
   pinMode(buttonPin1, INPUT);//Set D3 pin to input mode
   pinMode(buttonPin2, INPUT);//Set D4 pin to input mode
   pinMode(laserPin,OUTPUT);//Set the D12 pin to output mode
   digitalWrite(laserPin,HIGH);// Turn on the lasers !
+  delay(100);
+    Serial.begin(9600);
+}
+
+void readInput()
+{
+  for (int i = 0; i < 6; ++i)
+  {
+    char newState = analogRead(lasersPin[i]) < sensi;
+    lasersPressed[i] = newState && !lasersHold[i];
+    lasersHold[i] = newState;
+  }
+  // Since pin7 digital...
+  char newState = digitalRead(lasersPin[6]);
+  lasersPressed[6] = newState && !lasersHold[6];
+  lasersHold[6] = newState;
+}
+
+void loopSquare()
+{
+  for (int i = 0; i < 7; ++i)
+  {
+    if (lasersHold[i])
+      squareGenerator[i].volume = 127 / (sizeof(squareGenerator) / sizeof(squareGenerator[0]));
+    else
+      if (squareGenerator[i].volume > 0)
+        squareGenerator[i].volume--;
+  }
+}
+
+void loopPwm()
+{
+  for (int i = 0; i < 7; ++i)
+  {
+    pwmGenerator[i].pwmSweep();
+    if (lasersHold[i])
+    {
+      if (pwmGenerator[i].volume < 127 / (sizeof(pwmGenerator) / sizeof(pwmGenerator[0])))
+       pwmGenerator[i].volume++;
+    }
+    else
+      if (pwmGenerator[i].volume > 0)
+        pwmGenerator[i].volume--;
+  }
 }
 
 void loop()
 {
-  val=analogRead(pint);//Read the analog value of the photoresistor R1
-  val2=analogRead(pint2);//Read the analog value of the photoresistor R2
-  val3=analogRead(pint3);//Read the analog value of the photoresistor R3
-  val4=analogRead(pint4);//Read the analog value of the photoresistor R4
-  val5=analogRead(pint5);//Read the analog value of the photoresistor R5
-  val6=analogRead(pint6);//Read the analog value of the photoresistor R6
-  val7=digitalRead(pint7);//Read the digital value of the photoresistor R7
-
-  if (val < sensi)
-  {
-    squareGenerator[0].period = 20000 / 880;
-    squareGenerator[0].volume = 63;
-  }
-  else 
-  {
-    if (squareGenerator[0].volume > 0)
-    {
-      squareGenerator[0].volume--;
-    }
-  }
-  
-  if (val2 < sensi)
-  {
-    squareGenerator[1].period = 20000 / 750;
-    squareGenerator[1].volume = 63;
-  }
-  else 
-  {
-    if (squareGenerator[1].volume > 0)
-    {
-      squareGenerator[1].volume--;
-    }
-  }
-  delay(3);
+  readInput();
+  loopPwm();
+//if button loop square
+  delay(15);
 }
 
 /// --------------------------
 /// Custom ISR Timer Routine
 /// --------------------------
-void timerIsr()
+void timerIsrSquare()
 {
   Timer1.setPwmDuty(beep, (((int)totalValue) + 128) << 2);
-  squareGenerator[0].counterInc();
-  squareGenerator[1].counterInc();  
-  totalValue = squareGenerator[0].getValue() + squareGenerator[1].getValue();
+  totalValue = 0;
+  for (int i = 0; i < 7; ++i)
+  {
+    squareGenerator[i].counterInc();
+    totalValue += squareGenerator[i].getValue();
+  }
+}
+
+void timerIsrPwm()
+{
+  Timer1.setPwmDuty(beep, (((int)totalValue) + 128) << 2);
+  totalValue = 0;
+  for (int i = 0; i < 7; ++i)
+  {
+    pwmGenerator[i].counterInc();
+    totalValue += pwmGenerator[i].getValue();
+  }
 }
